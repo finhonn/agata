7210cc7f9f5cc9fcf727953515b122e6e975eccb
finhonn <dukinhapaneladepressaogmail.com>
2024-06-20 16:05:53 -0300

    Primeiro commit

---
+++ arquivoexemplo.java
@@ -0,0 +1,8 @@
+public class OlaMundo (
+		//metodo principal - inicia o aplicativo java
+
+		public static void main(String[] args){
+			//Exibe a Frase ("Ola Mundo");
+
+)//fim do metodo principal
+}// fim da classe "Ola mundo"
